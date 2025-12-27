29327252fe6d1806252b0a3a94bd7e7298cb0c40
Lucile-Prothon <lucile.prothon@edu.univ-fcomte.fr>
2025-12-27 13:50:00 +0100

    Modification valeur css accordéons

---
+++ src/js/script.js
@@ -0,0 +1,38 @@
+/* Menu*/
+const toggle = document.querySelector('.menu-btn');
+const nav = document.querySelector(".menu");
+
+toggle.addEventListener('click', () => {
+    const isOpen = toggle.getAttribute("aria-expanded") === "true";
+
+    toggle.setAttribute("aria-expanded", !isOpen);
+    nav.setAttribute("aria-hidden", isOpen);
+
+    document.body.classList.toggle('noscroll', !isOpen)
+});
+
+// *** ACCORDEON ***
+document.addEventListener('DOMContentLoaded', () => {
+    const summaries = document.querySelectorAll('details > summary');
+})
+
+function toggleDetails(summary) {
+    const details = summary.closest('details');
+    const allDetails = document.querySelectorAll('details');
+
+    // fermer les autres
+    allDetails.forEach(d => {
+        if (d !== details) d.open = false;
+    });
+
+
+    // basculer celui cliqué
+    details.open = !details.open;
+}
+
+summaries.forEach(summary => {
+    summary.addEventListener('click', (e) => {
+        e.preventDefault(); // on gère l'ouverture nous-mêmes
+        toggleDetails(e.currentTarget);
+    });
+})
